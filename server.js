0399452a6b5749233de055916537efce978e22c2
Yashraj Makwana <makwanay@pmc.local>
2025-06-29 18:08:21 +0530

    fix front-end

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ try {
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -66,7 +66,7 @@ app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
