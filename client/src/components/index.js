3b76096f8fce966b0608b2c9157c87e9ca9c8e66
Yashraj Makwana <makwanay@pmc.local>
2025-06-29 18:27:24 +0530

    took form row select up

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,9 @@
 export { default as Logo } from './Logo';
 export { default as FormRow } from './FormRow';
+export { default as FormRowSelect } from './formRowSelect';
 export { default as BigSidebar } from './BigSidebar';
 export { default as SmallSidebar } from './SmallSidebar';
 export { default as Navbar } from './Navbar';
-export { default as FormRowSelect } from './formRowSelect';
 export { default as JobsContainer } from './JobsContainer';
 export { default as SearchContainer } from './SearchContainer';
 export { default as StatItem } from './StatItem';
